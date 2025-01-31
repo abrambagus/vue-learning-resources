0ca5888cd5d9f63b3a5142471809c2171208b467
abram <abrambagus@gmail.com>
2025-01-31 14:44:07 +0700

    add styling and component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <ul>
-    <learning-resource
-      v-for="res in storedRecouces"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :link="res.link"
-    ></learning-resource>
-  </ul>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedRecouces"></stored-resources>
 </template>
 
 <script>
-import LearningResource from './component/leaning-resource/LearningResource.vue';
+import StoredResources from './component/leaning-resource/StoredReasources.vue';
+import TheHeader from './component/layouts/TheHeader.vue';
 
 export default {
   components: {
-    LearningResource,
+    StoredResources,
+    TheHeader,
   },
   data() {
     return {
@@ -37,3 +32,19 @@ export default {
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
