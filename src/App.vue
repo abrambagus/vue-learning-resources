d4e31896b278de32962f73c34e0d032bb3b18fd8
abram <abrambagus@gmail.com>
2025-01-31 10:39:25 +0700

    add first component & props

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedRecouces"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './component/leaning-resource/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedRecouces: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
